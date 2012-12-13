96d113d9604be32aa19db2d54b1bc2a5c302cd7d
Niklas <n.malmqvist@gmail.com>
2012-12-13 14:33:57 +0100

    Re-wrote most of the script.

--- src/bdfilter.py
+++ src/bdfilter.py
@@ -8,37 +8,121 @@ Created on Dec 5, 2012
 
 import re
 
-def main(inputFile, outputFile, minSamples, controlID_, controlFilter_, minScore_):
+def fixChrName(chrName):
+    chrMatch = re.match(r'chr(.*)', chrName)
+    if chrMatch:
+        return chrMatch.group(1)
+    else:
+        return chrName
+
+def createRefList(controlFile):
+    
+    refList = []
+    FH_CONTROL = open(controlFile, "rU")
+
+    for line in FH_CONTROL:
+        if not re.search(r'^#', line):
+
+            splitline = line.split('\t')
+            
+            chr1 = fixChrName(splitline[0])
+            start = float(splitline[1])
+            chr2 = fixChrName(splitline[3])
+            end = float(splitline[4])
+            
+            refList.append([chr1, start, chr2, end])
+
+    FH_CONTROL.close()
+    return sorted(refList)
+    
+
+def main(inputFile, outputFile, controlFile, minScore_):
     
     FH_INPUT = open(inputFile, "rU")
     FH_OUTPUT = open(outputFile, "w")
-    controlID = controlID_
-    controlFilter = controlFilter_
+    refList = createRefList(controlFile)
+    
+    nbrOfSVs = 0
+    overlaps = 1
     
     for line in FH_INPUT:
         
+        matched = "NA"
+        
         if not re.search(r'^#', line):
-    
+            
             splitline = line.split('\t')
+            chr1_in = fixChrName(splitline[0])
+            start_in = float(splitline[1])
+            chr2_in = fixChrName(splitline[3])
+            end_in = float(splitline[4])
             
-            noControls = 0
-            samples = splitline[10].split(':')
+            for ref in refList:
+                             
+                if ref[0] == chr1_in:
+
+                    ## Found the correct chromosome, compare start and end positions
+                                        
+                    ## Case I
+                    if ref[1] >= start_in and ref[1] <= end_in and ref[3] >= end_in:
+                        if matched == "NA" or matched == False:
+                            matched = True
+                            overlaps += 1
+                            overlap = str(round( (end_in-ref[1])/(end_in-start_in), 2))
+                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+                        
+                            print "Match: " + str(matched) + " sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
+                    
+                    ## Case II
+                    elif ref[3] <= end_in and ref[3] >= start_in and ref[1] <= start_in:
+                        if matched == "NA" or matched == False:
+                            matched = True
+                            overlaps += 1
+                            overlap = str(round( (ref[3]-start_in)/(end_in-start_in), 2))
+                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+                        
+                            print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
+                    
+                    ## Case III
+                    elif ref[1] <= start_in and ref[3] >= end_in:
+                        if matched == "NA" or matched == False:
+                            matched = True
+                            overlaps += 1
+                            overlap = str(1)
+                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+                        
+                            print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
+                        
+                    ## Case IV
+                    elif ref[1] >= start_in and ref[3] <= end_in:
+                        if matched == "NA" or matched == False:
+                            matched = True
+                            overlaps += 1
+                            overlap = str(round( (ref[3]-ref[1])/(end_in-start_in), 2))
+                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+                        
+                            print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
+
             
-            for sample in samples:
-                noControls += sample.count(controlID)
-            
-            noCases = len(splitline[10].split(':')) - noControls
-            svScore = splitline[8]
-            
-            if controlFilter:
-                if noCases == minSamples and noControls == 0:
-                    if svScore >= minScore_:
-                        FH_OUTPUT.write(line)
-            
-            else:
-                if noCases == minSamples:
-                    if svScore >= minScore_:
-                        FH_OUTPUT.write(line)
+                    ## Non-overlap
+                    else:
+                        #print "No match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[1]) + "-" + str(ref[3])
+                        if matched == "NA":
+                            matched = False
+
+                
+                
+            if matched == False:
+                overlap = str(0)
+                FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")        
+                print "No match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
+                                
+            nbrOfSVs += 1
+               
+    print "\nAll done!\n"
+    print "Overlaps:", overlaps
+    print "SVs in sample file:", nbrOfSVs
+    print "Overlap percentage:", str(round(float(overlaps)/float(nbrOfSVs), 2) * 100) + "%"
                         
     FH_INPUT.close()
     FH_OUTPUT.close()
--- src/main.py
+++ src/main.py
@@ -16,24 +16,27 @@ def main():
     
     argparser.add_argument("input",
                            help="Path to the input file.")
+
+    argparser.add_argument("control",
+                           help="A comparison file that contains SVs that should be disregarded if they exist in the input, i.e. a file with the SVs in the control group.")
     
     argparser.add_argument("output",
                            help="Path to the output file.")
     
-    argparser.add_argument("type",
-                           help="Input file type. [bd|bdf] (BreakDancer|BreakDancer_filtered).")
-    
-    argparser.add_argument("-minSamples",
-                           type=int,
-                           help="Minimum number of samples to share an SV.")
-    
-    argparser.add_argument("-controlID",
-                           help="A common name or partial name that can be used to identify the control samples.")
-    
-    argparser.add_argument("-controlFilter",
-                           action="store_true",
-                           help="Enable to exclude SVs that occurs in the controls.")
-
+#    argparser.add_argument("type",
+#                           help="Input file type. [bd|bdf] (BreakDancer|BreakDancer_filtered).")
+#    
+#    argparser.add_argument("-minSamples",
+#                           type=int,
+#                           help="Minimum number of samples to share an SV.")
+#    
+#    argparser.add_argument("-controlID",
+#                           help="A common name or partial name that can be used to identify the control samples.")
+#    
+#    argparser.add_argument("-controlFilter",
+#                           action="store_true",
+#                           help="Enable to exclude SVs that occurs in the controls.")
+#
     argparser.add_argument("-minScore",
                            type=int,
                            help="Minimum score threshold (1-99)")
@@ -42,28 +45,30 @@ def main():
     
     inputFile = args.input
     outputFile = args.output
-    inputType = args.type
-    minSamples = args.minSamples
-    controlID = args.controlID
+    controlFile = args.control
+#    inputType = args.type
+#    minSamples = args.minSamples
+#    controlID = args.controlID
     
     if args.minScore:
         minScore = args.minScore
     else:
         minScore = 30
+#    
+#    if args.controlFilter:
+#        controlFilter = True
+#    else:
+#        controlFilter = False
     
-    if args.controlFilter:
-        controlFilter = True
-    else:
-        controlFilter = False
-    
-    if inputType == "bd":
-        bdfilter.main(inputFile, outputFile, minSamples, controlID, controlFilter, minScore)
-    
+    #if inputType == "bd":
+#        bdfilter.main(inputFile, outputFile, minSamples, controlID, controlFilter, minScore)
+    bdfilter.main(inputFile, outputFile, controlFile, minScore)
+        
 #    elif inputType == "bdf":
 #        bdcompare.main(inputFile, outputFile, )
     
-    else:
-        sys.exit("Invalid input type: " + inputType)
+#    else:
+#        sys.exit("Invalid input type: " + inputType)
         
 if __name__ == '__main__':
     main()
