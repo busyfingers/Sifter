7e118ab1b7d6914f4785726810e434b316f8fdc8
Niklas <n.malmqvist@gmail.com>
2012-12-17 12:07:41 +0100

    Added support for UCSC-style file containing known variants

--- src/bdfilter.py
+++ src/bdfilter.py
@@ -7,6 +7,9 @@ Created on Dec 5, 2012
 '''
 
 import re
+import sys
+
+results = []
 
 def fixChrName(chrName):
     chrMatch = re.match(r'chr(.*)', chrName)
@@ -15,22 +18,40 @@ def fixChrName(chrName):
     else:
         return chrName
 
-def createRefList(controlFile):
-    print "[INFO] Preparing reference list ..."
+def createRefList(controlFile, type_):
+    print "[INFO] Preparing reference list (%s)..." % type_
     refList = []
     FH_CONTROL = open(controlFile, "rU")
 
-    for line in FH_CONTROL:
-        if not re.search(r'^#', line):
-
-            splitline = line.split('\t')
+    if type_ == "bd":
+        for line in FH_CONTROL:
+            if not re.search(r'^#', line):
+    
+                splitline = line.split('\t')
+                
+                chr1 = fixChrName(splitline[0])
+                start = float(splitline[1])
+                chr2 = fixChrName(splitline[3])
+                end = float(splitline[4])
+                
+                refList.append([chr1, start, chr2, end])
+    
+    elif type_ == "ucsc":
+        for line in FH_CONTROL:
+            if not re.search(r'^VariationID', line):
+    
+                splitline = line.split('\t')
+                
+                chr1 = fixChrName(splitline[2])
+                start = float(splitline[3])
+                chr2 = "NA"
+                end = float(splitline[4])
+                
+                refList.append([chr1, start, chr2, end])
             
-            chr1 = fixChrName(splitline[0])
-            start = float(splitline[1])
-            chr2 = fixChrName(splitline[3])
-            end = float(splitline[4])
-            
-            refList.append([chr1, start, chr2, end])
+    else:
+        sys.exit("Incorrect type: " + type_)
+        
 
     FH_CONTROL.close()
     return sorted(refList)
@@ -55,14 +76,60 @@ def keepOnlyCommon(inputFile_, minSamples_):
     FH_OUTPUT.close()
     return inputFile
 
-def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
-    refList = createRefList(controlFile_)
-    print "[INFO] Comparing SVs against reference control ..."
-    FH_INPUT = open(inputFile, "rU")
+def getRowInfo(splitline, type_):
+    if type_ == "bd":
+        chr1_in = fixChrName(splitline[0])
+        start_in = float(splitline[1])
+        chr2_in = fixChrName(splitline[3])
+        end_in = float(splitline[4])
+        return (chr1_in, start_in, chr2_in, end_in)
+    
+    elif type_ == "ucsc":
+        chr1_in = fixChrName(splitline[0])
+        start_in = float(splitline[1])
+        chr2_in = "NA"
+        end_in = float(splitline[4])    
+        return (chr1_in, start_in, chr2_in, end_in)
+    
+    else:
+        sys.exit("Incorrect type: " + type_)
+
+def writeOutput(outputFile_, results):
+
     FH_OUTPUT = open(outputFile_, "w")
+    
+    for result in results:
+        for element in result:
+            #print element
+            FH_OUTPUT.write(element)
+        FH_OUTPUT.write("\n")
+    
+    FH_OUTPUT.close()
+
+
+def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_, type_):
+    refList = createRefList(controlFile_, type_)
+    
+    if type_ == "bd":
+        print "[INFO] Comparing SVs against reference control ..."
+    
+    elif type_ == "ucsc":
+        print "[INFO] Comparing SVs against known variants ..."
+
+    FH_INPUT = open(inputFile, "rU")
+    
+#    if type_ == "bd":
+#        FH_OUTPUT = open(outputFile_, "w")
+#    
+#    elif type_ == "ucsc":
+#        FH_OUTPUT = open(outputFile_, "a")
+#
+#    
+#    else:
+#        sys.exit("Error opening output file." + type_)
      
     nbrOfSVs = 0
-    overlaps = 1
+    overlaps = 0
     
     for line in FH_INPUT:
         
@@ -72,15 +139,15 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
         matched = "NA"
         
         if not re.search(r'^#', line):
-            
             splitline = line.split('\t')
-            chr1_in = fixChrName(splitline[0])
-            start_in = float(splitline[1])
-            chr2_in = fixChrName(splitline[3])
-            end_in = float(splitline[4])
             
-
+            rowInfo = getRowInfo(splitline, type_)
             
+            chr1_in = fixChrName(rowInfo[0])
+            start_in = float(rowInfo[1])
+            chr2_in = "NA"
+            end_in = float(rowInfo[3])
+                       
             for ref in refList:
                              
                 if ref[0] == chr1_in:
@@ -92,7 +159,19 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
                             matched = True
                             overlaps += 1
                             overlap = str(round( (end_in-ref[1])/(end_in-start_in), 2))
-                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+                            
+                            rowRes = [line.rstrip("\n") + "\t" + overlap]
+                            
+                            if type_ == "bd":
+                                results.append(rowRes)
+                            elif type_ == "ucsc":
+                                results[nbrOfSVs].append("\t" + overlap)
+                            else:
+                                sys.exit("Error storing results. Exiting ...")
+                            
+                            #writeRow(FH_OUTPUT, overlap, line, type_)
+                            
+                            #FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
                         
                             #print "Match: " + str(matched) + " sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
                     
@@ -102,7 +181,19 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
                             matched = True
                             overlaps += 1
                             overlap = str(round( (ref[3]-start_in)/(end_in-start_in), 2))
-                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+                            
+                            rowRes = [line.rstrip("\n") + "\t" + overlap]
+                            
+                            if type_ == "bd":
+                                results.append(rowRes)
+                            elif type_ == "ucsc":
+                                results[nbrOfSVs].append("\t" + overlap)
+                            else:
+                                sys.exit("Error storing results. Exiting ...")                       
+                            
+                            #writeRow(FH_OUTPUT, overlap, line, type_)
+                            
+                            #FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
                         
                             #print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
                     
@@ -112,7 +203,19 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
                             matched = True
                             overlaps += 1
                             overlap = str(1)
-                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+
+                            rowRes = [line.rstrip("\n") + "\t" + overlap]
+                            
+                            if type_ == "bd":
+                                results.append(rowRes)
+                            elif type_ == "ucsc":
+                                results[nbrOfSVs].append("\t" + overlap)
+                            else:
+                                sys.exit("Error storing results. Exiting ...")
+                            
+                            #writeRow(FH_OUTPUT, overlap, line, type_)
+                            
+                            #FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
                         
                             #print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
                         
@@ -122,10 +225,21 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
                             matched = True
                             overlaps += 1
                             overlap = str(round( (ref[3]-ref[1])/(end_in-start_in), 2))
-                            FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
+
+                            rowRes = [line.rstrip("\n") + "\t" + overlap]
+                            
+                            if type_ == "bd":
+                                results.append(rowRes)
+                            elif type_ == "ucsc":
+                                results[nbrOfSVs].append("\t" + overlap)
+                            else:
+                                sys.exit("Error storing results. Exiting ...")
+                            
+                            #writeRow(FH_OUTPUT, overlap, line, type_)
+                            
+                            #FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")
                         
-                            #print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
-    
+                            #print "Match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)   
             
                     ## Non-overlap
                     else:
@@ -140,7 +254,19 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
                 
             if matched == False:
                 overlap = str(0)
-                FH_OUTPUT.write(line.rstrip("\n")[1:] + "\t" + overlap + "\n")        
+                
+                rowRes = [line.rstrip("\n") + "\t" + overlap]
+                
+                if type_ == "bd":
+                    results.append(rowRes)
+                elif type_ == "ucsc":
+                    results[nbrOfSVs].append("\t" + overlap)
+                else:
+                    sys.exit("Error storing results. Exiting ...")
+
+                #writeRow(FH_OUTPUT, overlap, line, type_)
+                
+                #FH_OUTPUT.write(line.rstrip("\n") + "\t" + overlap + "\n")        
                 #print "No match: sample " + str(chr1_in) + " " + str(start_in) + "-" + str(end_in) + " ref " + str(ref[0]) + " " + str(ref[1]) + "-" + str(ref[3]) + " Overlap: " + str(overlap)
                                 
             nbrOfSVs += 1
@@ -151,18 +277,26 @@ def checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_):
     print "[INFO] Overlap percentage:", str(round(float(overlaps)/float(nbrOfSVs), 2) * 100) + "%\n"
                         
     FH_INPUT.close()
-    FH_OUTPUT.close()
 
-def main(inputFile_, outputFile_, controlFile_, minScore_, minSamples_):
+
+
+def main(inputFile_, outputFile_, controlFile_, minScore_, minSamples_, known_):
     
     if minSamples_ > 1:
 
         inputFile = keepOnlyCommon(inputFile_, minSamples_)
-        checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_)
+        checkAllSVs(inputFile, outputFile_, controlFile_, minScore_, minSamples_, "bd")
+        if known_:
+            checkAllSVs(inputFile, outputFile_, known_, minScore_, minSamples_, "ucsc")
+        
     
     else:
-        checkAllSVs(inputFile_, outputFile_, controlFile_, minScore_, minSamples_)
+        checkAllSVs(inputFile_, outputFile_, controlFile_, minScore_, minSamples_, "bd")
+        if known_:
+            checkAllSVs(inputFile, outputFile_, known_, minScore_, minSamples_, "ucsc")
 
+
+    writeOutput(outputFile_, results)
     
 if __name__ == '__main__':
     main()
--- src/main.py
+++ src/main.py
@@ -22,21 +22,15 @@ def main():
     
     argparser.add_argument("output",
                            help="Path to the output file.")
-    
-#    argparser.add_argument("type",
-#                           help="Input file type. [bd|bdf] (BreakDancer|BreakDancer_filtered).")
-#    
+      
     argparser.add_argument("-minSamples",
                            type=int,
                            help="Minimum number of samples to share an SV.")
-#    
-#    argparser.add_argument("-controlID",
-#                           help="A common name or partial name that can be used to identify the control samples.")
-#    
-#    argparser.add_argument("-controlFilter",
-#                           action="store_true",
-#                           help="Enable to exclude SVs that occurs in the controls.")
-#
+ 
+    argparser.add_argument("-known",
+                           help="Path to a file containing known SVs.")
+
+    
     argparser.add_argument("-minScore",
                            type=int,
                            help="Minimum score threshold (1-99)")
@@ -46,32 +40,24 @@ def main():
     inputFile = args.input
     outputFile = args.output
     controlFile = args.control
-#    inputType = args.type
+    
+    if args.known:
+        known = args.known
+    else:
+        known = 0
+    
     if args.minSamples:
         minSamples = args.minSamples
     else:
         minSamples = 1
-#    controlID = args.controlID
     
     if args.minScore:
         minScore = args.minScore
     else:
         minScore = 30
-#    
-#    if args.controlFilter:
-#        controlFilter = True
-#    else:
-#        controlFilter = False
-    
-    #if inputType == "bd":
-#        bdfilter.main(inputFile, outputFile, minSamples, controlID, controlFilter, minScore)
-    bdfilter.main(inputFile, outputFile, controlFile, minScore, minSamples)
+
+    bdfilter.main(inputFile, outputFile, controlFile, minScore, minSamples, known)
         
-#    elif inputType == "bdf":
-#        bdcompare.main(inputFile, outputFile, )
-    
-#    else:
-#        sys.exit("Invalid input type: " + inputType)
         
 if __name__ == '__main__':
     main()
